e13865cfcf5c3f097a710be5bf4d3e05cc9e8d84
Mopsy_14_Desktop <redstone.computerboy@gmail.com>
2023-08-20 21:08:24 +0200

    V13.0.0 BungeeCord Finished BungeeCord port.

--- src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
+++ src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
@@ -1,9 +1,18 @@
 package mopsy.productions.discord.statusbot;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 
-public class StatusbotMain {
-    public void init(){initAll();}
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class StatusbotMain extends Plugin implements Listener {
     private void initAll(){
         ConfigManager.init(this);
     }
@@ -11,7 +20,57 @@ public class StatusbotMain {
 
     }
     String getConfigPath(){
-        return "";
+        return getDataFolder().getAbsolutePath();
     }
 
+    private List<String> MakeStringList(Collection<ProxiedPlayer> players){
+        List<String> res = new ArrayList<>(players.size());
+        for(ProxiedPlayer player : players){
+            res.add(player.getName());
+        }
+        return res;
+    }
+    private List<String> MakeStringList(Collection<ProxiedPlayer> players, String excluded){
+        List<String> res = new ArrayList<>(players.size()-1);
+        for(ProxiedPlayer player : players){
+            String name = player.getName();
+            if(!name.equals(excluded)){
+                res.add(name);
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public void onEnable() {
+        initAll();
+        getProxy().getPluginManager().registerListener(this,this);
+
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(MakeStringList(getProxy().getPlayers()))
+        );
+    }
+
+    @EventHandler
+    public void onJoin(ServerConnectedEvent e){
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(MakeStringList(getProxy().getPlayers()))
+        );
+    }
+    @EventHandler
+    public void onLeave(PlayerDisconnectEvent e){
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(MakeStringList(getProxy().getPlayers(),e.getPlayer().getName()))
+        );
+    }
+
+    @Override
+    public void onDisable() {
+        if(BotManger.jda!=null){
+            BotManger.jda.shutdownNow();
+        }
+    }
 }
