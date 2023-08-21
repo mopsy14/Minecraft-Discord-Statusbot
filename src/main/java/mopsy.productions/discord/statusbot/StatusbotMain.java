c9b042ba56ec8ffe2ac98eac78d157c09416f145
Mopsy_14_Desktop <redstone.computerboy@gmail.com>
2023-08-21 14:41:35 +0200

    V13.0 Fabric Finished the Fabric version of the framework

--- src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
+++ src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
@@ -1,9 +1,18 @@
 package mopsy.productions.discord.statusbot;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 
-public class StatusbotMain {
-    public void init(){initAll();}
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+public class StatusbotMain implements ModInitializer {
+
+    private String[] prevPlayers = {};
+
     private void initAll(){
         ConfigManager.init(this);
     }
@@ -11,7 +20,34 @@ public class StatusbotMain {
 
     }
     String getConfigPath(){
-        return "";
+        return System.getProperty("user.dir") + File.separator + "config";
     }
 
+
+    @Override
+    public void onInitialize() {
+        initAll();
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server->{
+            prevPlayers = server.getPlayerNames();
+            BotManger.regBot(
+                    ConfigManager.configuration.getString("bot_token"),
+                    Parser.createStatusMessage(List.of(prevPlayers))
+            );
+        });
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+            if (BotManger.jda != null) {
+                BotManger.jda.shutdownNow();
+            }
+        });
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            if(!Arrays.equals(server.getPlayerNames(), prevPlayers)){
+                prevPlayers = server.getPlayerNames();
+                BotManger.regBot(
+                        ConfigManager.configuration.getString("bot_token"),
+                        Parser.createStatusMessage(List.of(prevPlayers))
+                );
+            }
+        });
+    }
 }
