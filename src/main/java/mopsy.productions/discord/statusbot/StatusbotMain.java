bca23dedb813feac81d16938f6ecdcef7240c8c5
mopsy14 <Redstone.computerboy@gmail.com>
2023-10-10 22:01:07 +0200

    V13.0.0 Forge Did most of the work for porting the framework to forge, however getting the dependencies working in the artifact seems a bit more difficult...

--- src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
+++ src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
@@ -1,9 +1,21 @@
 package mopsy.productions.discord.statusbot;
 
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.PlayerLoggedOutEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.util.Arrays;
+
+@Mod("discord_statusbot")
 public class StatusbotMain {
-    public void init(){initAll();}
+    private MinecraftServer server = null;
     private void initAll(){
         ConfigManager.init(this);
     }
@@ -11,7 +23,49 @@ public class StatusbotMain {
 
     }
     String getConfigPath(){
+        if(server != null && server.isDedicatedServer()){
+            return server.getServerDirectory().getAbsolutePath() + File.separator + "config";
+        }
         return "";
+    }
+
+
+    public StatusbotMain() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void start(final FMLServerStartedEvent event) {
+        server = event.getServer();
+        initAll();
+
+
+
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(Arrays.asList(event.getServer().getPlayerNames()))
+        );
+    }
+
+    @SubscribeEvent
+    public void stop(final FMLServerStoppingEvent event){
+        BotManger.jda.shutdownNow();
+    }
+
+    @SubscribeEvent
+    public void joined(final PlayerLoggedInEvent event) {
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(Arrays.asList(server.getPlayerNames()))
+        );
+    }
+
+    @SubscribeEvent
+    public void left(final PlayerLoggedOutEvent event) {
+        BotManger.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(Arrays.asList(server.getPlayerNames()))
+        );
     }
 
 }
