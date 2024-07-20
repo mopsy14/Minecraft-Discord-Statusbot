2081bbc97d07d2add936c6f38d5df3cf6efd08ba
mopsy14 <Redstone.computerboy@gmail.com>
2024-07-20 20:37:07 +0200

    V18.0.0 NeoForge_1.20.6 Made a version for NeoForge 1.20.2+

--- src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
+++ src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
@@ -1,9 +1,26 @@
 package mopsy.productions.discord.statusbot;
 
+import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.minecraft.server.MinecraftServer;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Mod("statusbot")
 public class StatusbotMain {
-    public void init(){initAll();}
+    private MinecraftServer server = null;
     private void initAll(){
         ConfigManager.init(this);
         DataManager.getAllData(this);
@@ -12,9 +29,163 @@ public class StatusbotMain {
 
     }
     String getConfigPath(){
+        if(server != null && server.isDedicatedServer()){
+            return Path.of("").toAbsolutePath() + File.separator + "config";
+        }
         return "";
     }
     public void onBotReady(){
+        if(BotManager.jda!=null) {
+            if (ConfigManager.getBool("enable_server_start_messages")) {
+                String startMessage = Parser.createStartMessage();
+                if (ConfigManager.getBool("enable_direct_message_status_messages")) {
+                    for (long id : BotManager.messageTextChannels) {
+                        TextChannel channel = BotManager.jda.getTextChannelById(id);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+                    }
+                }
+                if (ConfigManager.getBool("enable_text_channel_status_messages")) {
+                    for (UserChannelPair id : BotManager.messagePrivateChannels) {
+                        PrivateChannel channel = BotManager.jda.getPrivateChannelById(id.channel);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+                    }
+                }
+            }
+        }
+    }
+    public StatusbotMain() {
+        NeoForge.EVENT_BUS.register(this);
+    }
+    @SubscribeEvent
+    public void start(final ServerStartedEvent event) {
+        server = event.getServer();
+        initAll();
 
+
+
+        BotManager.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(()-> Arrays.asList(server.getPlayerNames()),server.getPlayerNames().length),
+                this
+        );
+    }
+
+    @SubscribeEvent
+    public void stop(final ServerStoppingEvent event){
+        if(BotManager.jda!=null){
+            if (ConfigManager.getBool("enable_server_stop_messages")) {
+                if (ConfigManager.getBool("enable_direct_message_status_messages")) {
+                    String stopMessage = Parser.createStopMessage();
+                    for (long id : BotManager.messageTextChannels) {
+                        TextChannel channel = BotManager.jda.getTextChannelById(id);
+                        if (channel != null)
+                            channel.sendMessage(stopMessage).queue();
+                    }
+                }
+                if (ConfigManager.getBool("enable_text_channel_status_messages")) {
+                    String stopMessage = Parser.createStopMessage();
+                    for (UserChannelPair id : BotManager.messagePrivateChannels) {
+                        PrivateChannel channel = BotManager.jda.getPrivateChannelById(id.channel);
+                        if (channel != null)
+                            channel.sendMessage(stopMessage).queue();
+                    }
+                }
+            }
+        }
+        DataManager.saveAllData(this);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if(BotManager.jda!=null){
+            BotManager.jda.shutdown();
+
+            try {
+                if(!BotManager.jda.awaitShutdown(Duration.ofSeconds(10))){
+                    BotManager.jda.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+    @SubscribeEvent
+    public void joined(final PlayerEvent.PlayerLoggedInEvent event) {
+        BotManager.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(()->Arrays.asList(server.getPlayerNames()),server.getPlayerNames().length),
+                this
+        );
+        if(BotManager.jda!=null) {
+            if (ConfigManager.getBool("enable_server_join_messages")) {
+                String startMessage = Parser.createJoinMessage(
+                        ()->Arrays.asList(server.getPlayerNames()),
+                        event.getEntity().getName().getString(),
+                        server.getPlayerNames().length
+                );
+                if (ConfigManager.getBool("enable_direct_message_status_messages")) {
+                    for (long id : BotManager.messageTextChannels) {
+                        TextChannel channel = BotManager.jda.getTextChannelById(id);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+
+                    }
+                }
+                if (ConfigManager.getBool("enable_text_channel_status_messages")) {
+                    for (UserChannelPair id : BotManager.messagePrivateChannels) {
+                        PrivateChannel channel = BotManager.jda.getPrivateChannelById(id.channel);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void left(final PlayerEvent.PlayerLoggedOutEvent event) {
+        BotManager.regBot(
+                ConfigManager.configuration.getString("bot_token"),
+                Parser.createStatusMessage(()->makeStringList(server.getPlayerNames(), event.getEntity().getName().getString()),server.getPlayerNames().length-1),
+                this
+        );
+        if(BotManager.jda!=null) {
+            if (ConfigManager.getBool("enable_server_leave_messages")) {
+                String startMessage = Parser.createLeaveMessage(
+                        ()->makeStringList(server.getPlayerNames(),event.getEntity().getName().getString()),
+                        event.getEntity().getName().getString(),
+                        server.getPlayerNames().length-1
+                );
+                if (ConfigManager.getBool("enable_direct_message_status_messages")) {
+                    for (long id : BotManager.messageTextChannels) {
+                        TextChannel channel = BotManager.jda.getTextChannelById(id);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+
+                    }
+                }
+                if (ConfigManager.getBool("enable_text_channel_status_messages")) {
+                    for (UserChannelPair id : BotManager.messagePrivateChannels) {
+                        PrivateChannel channel = BotManager.jda.getPrivateChannelById(id.channel);
+                        if (channel != null)
+                            channel.sendMessage(startMessage).queue();
+                    }
+                }
+            }
+        }
+    }
+
+    private List<String> makeStringList(String[] players, String excluded){
+        List<String> res = new ArrayList<>(players.length-1);
+        for(String player : players){
+            if(!player.equals(excluded)){
+                res.add(player);
+            }
+        }
+        return res;
     }
 }
