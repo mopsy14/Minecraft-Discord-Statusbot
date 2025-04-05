fe2b60e9fa9c08ea23544432042e6be5110abe3a
Mopsy <redstone.computerboy@gmail.com>
2025-04-05 19:47:06 +0200

    V20.0.1 Spigot Changed version number, added all placeholders and implemented the embed features. Todo: Updating and testing JDA and SimpleYAML.

--- src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
+++ src/main/java/mopsy.productions/discord/statusbot/StatusbotMain.java
@@ -1,13 +1,17 @@
 package mopsy.productions.discord.statusbot;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import okhttp3.OkHttpClient;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.simpleyaml.configuration.file.YamlFile;
 
 import java.time.Duration;
@@ -28,17 +32,21 @@ public class StatusbotMain extends JavaPlugin implements Listener {
                 "",
                 "Options are true/false",
                 "This will enable or disable the sending of the player leave message in both server and private channels."));
-        ConfigManager.addConfigKey(configuration,"embed_content",String.join(
-                "\n",
-                "status: $server-status$"),
+        ConfigManager.addConfigKey(configuration,"embed_content",String.join("\n",
+                        "status: $server-status$",
+                        "$amount-of-players$/$max-players$ players online:",
+                        "$player-list$"),
                 String.join(
                     "\n",
                     "",
                     "This is the text displayed below the title of embeds",
                     "Possible placeholders are:",
-                    "$CPL$ the name of the player that joined",
-                    "$AOP$ being the number of players currently online on the server",
-                    "$PL$ being a list of player names separated by 'embed_player_separator_text'"));
+                    "$server-status$ A red (offline) or green (online) circle telling whether the server is online",
+                    "$amount-of-players$ The number of players currently online on the server",
+                    "$max-players$ The maximum number of players that can play on the server",
+                    "$motd$ The message of the day of the server",
+                    "$player-list$ A list of player names separated by 'embed_player_separator_text'",
+                    "$%ph-api%$ Replace ph-api with any placeholder of the PlaceholderAPI plugin (if installed)"));
         ConfigManager.addConfigKey(configuration,"embed_player_separator_text",", ",
                 String.join(
                         "\n",
@@ -169,6 +177,8 @@ public class StatusbotMain extends JavaPlugin implements Listener {
                     }
                 }
             }
+            online=false;
+            EmbedManager.tryUpdateAllEmbeds(this);
         }
         DataManager.saveAllData(this);
         try {
@@ -186,11 +196,20 @@ public class StatusbotMain extends JavaPlugin implements Listener {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-
+            OkHttpClient client = BotManager.jda.getHttpClient();
+            client.connectionPool().evictAll();
+            client.dispatcher().executorService().shutdownNow();
         }
     }
     public void onBotReady(){
         if(BotManager.jda!=null) {
+            if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI"))
+                EmbedManager.regBackupVarSupplier(((statusbotMain, input) -> {
+                    if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI"))
+                        return PlaceholderAPI.setPlaceholders(null,input);
+                    else
+                        return "N/A";
+                }));
             if (ConfigManager.getBool("enable_server_start_messages")) {
                 String startMessage = Parser.createStartMessage();
                 if (ConfigManager.getBool("enable_text_channel_status_messages")) {
@@ -209,9 +228,19 @@ public class StatusbotMain extends JavaPlugin implements Listener {
                     }
                 }
             }
+            new BukkitRunnable(){
+                @Override
+                public void run() {
+                    EmbedManager.tryUpdateAllEmbeds(StatusbotMain.this);
+                }
+            }.runTaskTimer(this,0,200);
         }
     }
     public void regDefaultEmbedVarProviders(){
         EmbedManager.regVarSupplier("server-status",(statusbotMain) -> statusbotMain.online?":green_circle:":":red_circle:");
+        EmbedManager.regVarSupplier("amount-of-players",(statusbotMain -> String.valueOf(getServer().getOnlinePlayers().size())));
+        EmbedManager.regVarSupplier("player-list",(statusbotMain -> String.join(ConfigManager.getStr("embed_player_separator_text"),MakeStringList(getServer().getOnlinePlayers().toArray()))));
+        EmbedManager.regVarSupplier("max-players",(statusbotMain -> String.valueOf(getServer().getMaxPlayers())));
+        EmbedManager.regVarSupplier("motd",(statusbotMain -> getServer().getMotd()));
     }
 }
